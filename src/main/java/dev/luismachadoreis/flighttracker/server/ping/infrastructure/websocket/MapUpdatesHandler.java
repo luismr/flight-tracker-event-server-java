c22f94751ef0acdcc0ceea3530ebb964b7bbc53f
Luis Machado Reis <luis.reis@singularideas.com.br>
2025-05-05 22:11:00 -0300

    Optmizing imports

--- src/main/java/dev/luismachadoreis/flighttracker/server/ping/infrastructure/websocket/MapUpdatesHandler.java
+++ src/main/java/dev/luismachadoreis/flighttracker/server/ping/infrastructure/websocket/MapUpdatesHandler.java
@@ -1,9 +1,7 @@
 package dev.luismachadoreis.flighttracker.server.ping.infrastructure.websocket;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import org.springframework.stereotype.Component;
